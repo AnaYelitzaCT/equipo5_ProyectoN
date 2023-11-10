56caa8859d613564d648fec3e3c1c2375045d7ee
tania <tania.cardona.cardenas@gmail.com>
2023-11-10 17:40:03 -0600

    Se agrego botón cloudify sin funcionamiento +js con datos de cuenta creada

--- Veterinaria/js/formulario.js
+++ Veterinaria/js/formulario.js
@@ -1,1 +1,13 @@
-let infoBoton = document.getElementById("reset");
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dginlkrcz', 
+    uploadPreset: 'tiendaMascotitas'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+      }
+    }
+  )
+  
+  document.getElementById("upload_widget").addEventListener("click", function(){
+      myWidget.open();
+    }, false);
+  
