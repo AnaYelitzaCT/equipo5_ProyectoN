f8f0fac309c9aed7431b7c820b573517cafe3476
tania <tania.cardona.cardenas@gmail.com>
2023-11-11 23:05:18 -0600

    se modificó botones, sin funcionamiento cloudinary

--- Veterinaria/js/formulario.js
+++ Veterinaria/js/formulario.js
@@ -1,13 +1,54 @@
-var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'dginlkrcz', 
-    uploadPreset: 'tiendaMascotitas'}, (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Done! Here is the image info: ', result.info); 
-      }
-    }
-  )
+
+`use strict`;
+const boton_subirFoto=  document.querySelector(`#upload_widget`);
+let widget_cloudinary= cloudinary.createUploadWidget({
+  cloudName: `dginlkrcz`,
+  uploadPresent: `tiendaMascotitas`
+
+}, (err, result) => {
+  if (!err && result & result.event === `success`){
+    console.log(`Imagen subida con éxito`, result.info);
+  }
+
   
-  document.getElementById("upload_widget").addEventListener("click", function(){
-      myWidget.open();
-    }, false);
+
+});
+boton_subirFoto.addEventListener(`click`, ()=>{
+
+});
+
+
+// import "./styles.css"
+
+// document.getElementById("app").innerHTML=`
+// <h1>Upload Widget</h1>
+// <div>
+// <button 
+// id="upload_widget"
+// class="cloudinary-button">
+// Upload files
+// </button>
+// </div>
+// `;
+
+// var myWidget = cloudinary.createUploadWidget(
+//   {
+//     cloudName: 'dginlkrcz', 
+//     uploadPreset: 'tiendaMascotitas'
+//   }, 
+//   (error, result) => { 
+//       if (!error && result && result.event === "success") { 
+//         console.log('Done! Here is the image info: ', result.info); 
+//       }
+//     }
+//   );
   
+//    document.getElementById("upload_widget").addEventListener(
+//     "click", 
+//     function(){
+//       myWidget.open();
+//    },
+//     false
+//     );
+  
+
